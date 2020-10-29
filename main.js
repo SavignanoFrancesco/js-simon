7735e97dc42ab93066ae8253fb472e734d543faf
Francesco <francesco.savignano96@gmail.com>
2020-10-29 13:44:46 +0100

    countdown timer impostato e stampato su pagina

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-$(document).ready(){
+$(document).ready(function(){
 
-}
+    var count = 30;
+
+    var countdown = setInterval(function() {
+
+        $('.timer').text(count);
+
+        count--;
+
+        if (count < 0) {
+            clearInterval(countdown);
+        }
+
+  }, 1000);
+
+});
