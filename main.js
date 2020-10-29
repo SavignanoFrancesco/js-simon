83e014a7a7d442fb5403482c08fe46ff5d2a9f5a
Francesco <francesco.savignano96@gmail.com>
2020-10-29 17:02:21 +0100

    aggiunta generazione di 5 numeri random, la loro stampa in pagina e la loro scomparsa dopo 30 secondi

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
+    var numbers = [];
+    var numbers_quantity = 5;
+    var i = 0;
+    var min = 1;
+    var max = 50;
+
+    do{
+        console.log(numbers.length);
+        console.log(numbers);
+        var rnd_number = getRndInteger(min,max);
+
+            numbers.push(rnd_number);
+
+    }while(numbers.length <= numbers_quantity);
+
+    for (var i = 0; i < numbers.length; i++) {
+        $('.numbers').append('<div>' + numbers[i] + '</div>');
+    }
+
+
+
     var count = 30;
 
     var countdown = setInterval(function() {
@@ -10,8 +31,16 @@ $(document).ready(function(){
 
         if (count < 0) {
             clearInterval(countdown);
+            $('.numbers').addClass('hide');
         }
 
   }, 1000);
 
 });
+
+//__________FUNZIONI___________
+
+//rndNumber generator
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
